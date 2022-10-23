2174c882fe10e7af767664f66bb86b0556acda98
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-23 13:55:38 +0000

    create User model and related files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    # sign up 時に name カラムにデータを保存することを許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
