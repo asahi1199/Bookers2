67a5457c673ee48921ab235f3bf22b00115697a1
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-25 14:18:56 +0000

    create books controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # association
-  has_many: :books, dependent: :destroy
+  has_many :books, dependent: :destroy
 
   # active strage
   has_one_attached :image
