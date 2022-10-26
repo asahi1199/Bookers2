a194c3575f362a3f72b802a98782c65bbf642fd7
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-26 13:19:08 +0000

    create book edit page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
     if @book.save()
       flash[:notice] = "Book was successfully created."
-      redirect_to "/books/#{@book.id}"
+      redirect_to book_path(@book)
     else
       @books = Book.all()
       render :index
@@ -19,6 +19,27 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy()
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
