8ed52562b683a411b471db260c2f24cb8fef5822
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-26 13:49:08 +0000

    user temporaly saving

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
+  resources :users, only: [:show, :edit]
 
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
