b4f5732ffe56c0afae95e039745c08be95f2db63
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-27 14:08:27 +0000

    validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # sign in 後のパス指定
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  # validation
+   validates :title, presence: true
+  validates :body, presence: true, length: {maximum: 200}
+
   # association
   belongs_to :user
 end
