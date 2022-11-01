59ee7bef96f9729ce31e8927aa66a2dde4d677cc
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-11-01 15:34:59 +0000

    modify for test

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,7 @@ class Favorite < ApplicationRecord
   # association
   belongs_to :user
   belongs_to :book
+
+  # validation (book_id と user_id の組み合わせの重複がないように)
+  validates_uniqueness_of :book_id, scope: :user_id
 end
